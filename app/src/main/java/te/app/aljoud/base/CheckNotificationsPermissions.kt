101d2a4bcb7f111d3064ae9249c5381a28b9d013
4msystems <4msystems400@gmail.com>
2023-03-10 09:57:30 +0200

    fix issues

---
+++ app/src/main/java/te/app/aljoud/base/CheckNotificationsPermissions.kt
@@ -0,0 +1,59 @@
+package te.app.aljoud.base
+
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.os.Build
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import te.app.aljoud.R
+import te.app.aljoud.utils.Constants.MY_PERMISSIONS_REQUEST_POST_NOTIFICATIONS
+
+object CheckNotificationsPermissions {
+    fun checkNotificationsPermissions(activity: Activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    activity,
+                    Manifest.permission.POST_NOTIFICATIONS
+                )
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                // Permission is not granted
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        activity,
+                        Manifest.permission.POST_NOTIFICATIONS
+                    )
+                ) {
+                    // Show an explanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+
+                    //to simplify, call requestPermissions again
+                    Toast.makeText(
+                        activity,
+                        activity.getString(R.string.permission_dialog_content1),
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                    ActivityCompat.requestPermissions(
+                        activity,
+                        arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                        MY_PERMISSIONS_REQUEST_POST_NOTIFICATIONS
+                    )
+                } else {
+                    // No explanation needed; request the permission
+                    ActivityCompat.requestPermissions(
+                        activity,
+                        arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                        MY_PERMISSIONS_REQUEST_POST_NOTIFICATIONS
+                    )
+                }
+            } else {
+                // permission granted
+
+            }
+        }
+    }
+}
